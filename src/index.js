6e5a4bfaa0e2e61665d1a58c81d3f9118a86ab08
Patrick Carlson <pats.carlson@gmail.com>
2017-11-12 19:24:37 -0500

    all this

--- src/index.js
+++ src/index.js
@@ -1,8 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Container from 'muicss/lib/react/container';
+import Row from 'muicss/lib/react/row';
+import Col from 'muicss/lib/react/col';
+import Button from 'muicss/lib/react/button';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// import registerServiceWorker from './registerServiceWorker
+// let imageUrl = 'bg.jpg';
+
+
+
+let continerStyle = {
+	marginLeft:"0px",
+	marginRight: "0px",
+	marginTop: "10px",
+	paddingTop: "17px",
+}
+
+// let navStyle = {
+// 	// background:"white",
+// 	border: "2px solid red"
+// };
+export default class App extends React.Component{
+	render() {
+		return(
+			<div >
+				<TopBar />
+				<br />
+				<div style={continerStyle}>
+					<Home name="Patrick" />
+				</div>
+			</div>
+		);
+	}
+}
+
+function TopBar() {
+	return (
+		<ul>
+		  <li><a href="#home">Home</a></li>
+		  <li><a href="#news">News</a></li>
+		  <li><a href="mailto:pats.carlson@gmail.com">Contact</a></li>
+		  <li><a href="https://github.com/Juniped" target="_blank">GitHub</a></li>
+		</ul>
+	)
+}
+
+function Home(props) {
+	return (
+			<h1 align="Center"> Home</h1>
+	);
+}
+
+
+ReactDOM.render(
+	<App />, 
+	document.getElementById('root')
+);
+// registerServiceWorker();
