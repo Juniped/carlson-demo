9c707ca6cfa045416b7a297ed7f7a389667d0ad4
Patrick Carlson <pats.carlson@gmail.com>
2017-11-12 20:48:22 -0500

    Derp

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 align="center">Home</h1>
+        <p>This site is here is my example/personal project work and coding practice.</p>
+ 
+        <p>Feel free to take a look around</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home
