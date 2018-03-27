21000eab52383e68c9b36de76cd1bb9baee06cee
Patrick Carlson <be.prepared@hotmail.com>
2018-03-27 02:04:57 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -51,7 +51,12 @@ function TopBar() {
 	return (
         <nav class="navbar" style={navbarStyle} role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
-
+              <div className="navbar-burger is-hidden-tablet">
+				        <span >Home></span>
+				        <span >Resume</span>
+				        <span >Contact></span>
+				        <span>github</span>
+			</div>
             </div>
             <div className="navbar-menu is-hidden-mobile">
         		<li className="top-bar"><NavLink className="top-bar" to="/">Home</NavLink></li>
@@ -63,12 +68,7 @@ function TopBar() {
             		</a>
     	        </li>
             </div>
-			<div className="navbar-burger is-hidden-tablet">
-				<span >Home></span>
-				<span >>Resume</span>
-				<span >Contact></span>
-				<span>github</span>
-			</div>
+			
 
             </nav>
 	);
